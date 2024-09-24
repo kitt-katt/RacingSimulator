98da9dbcd14b2fa8ecc216686be5570f708667ed
di5connect <d15c0nn3ct.post@gmail.com>
2024-09-24 19:14:23 +0300

    Создание класса гонки

--- Models/Raice.cs
+++ Models/Raice.cs
@@ -3,4 +3,52 @@
 
 namespace RacingSimulator.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public enum RaceType
+    {
+        Ground,
+        Air,
+        Mixed
+    }
+
+    public class Race
+    {
+        public double Distance { get; }
+        public RaceType Type { get; }
+        private List<Transport> _participants = new List<Transport>();
+
+        public Race(double distance, RaceType type)
+        {
+            Distance = distance;
+            Type = type;
+        }
+
+        public void RegisterTransport(Transport transport)
+        {
+            if ((Type == RaceType.Ground && transport is GroundTransport) ||
+                (Type == RaceType.Air && transport is AirTransport) ||
+                Type == RaceType.Mixed)
+            {
+                _participants.Add(transport);
+            }
+            else
+            {
+                throw new InvalidOperationException($"Транспорт {transport.Name} не может участвовать в гонке типа {Type}.");
+            }
+        }
+
+        public Transport StartRace()
+        {
+            if (_participants.Count == 0)
+            {
+                throw new InvalidOperationException("Невозможно начать гонку без участников.");
+            }
+
+            return _participants.OrderBy(t => t.CalculateRaceTime(Distance)).First();
+        }
+    }
 }
+
--- obj/Debug/net8.0/lab1.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828826e67a22cace37ab1e5529b63438985f4ac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
