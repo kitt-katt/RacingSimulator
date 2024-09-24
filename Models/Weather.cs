3e9643414cb9c12de4bc3a20124826562c420beb
di5connect <d15c0nn3ct.post@gmail.com>
2024-09-25 00:11:43 +0300

    Добавлен класс погоды, определяющий возможные типы погоды их влияние на различные ТС

---
+++ Models/Weather.cs
@@ -0,0 +1,56 @@
+namespace RacingSimulator.Models
+{
+    public enum WeatherType
+    {
+        // возможные типы погодных условий
+        Sunny,
+        Rainy,
+        Windy,
+        Foggy
+    }
+
+    public class Weather
+    {
+        public WeatherType Type { get; }
+
+        public Weather(WeatherType type)
+        {
+            Type = type;
+        }
+
+        // Определим влияние погоды на скорость транспорта в зависимости от его типа
+        public double GetSpeedModifier(Transport transport)
+        {
+            return transport switch
+            {
+                GroundTransport => GetGroundTransportModifier(),
+                AirTransport => GetAirTransportModifier(),
+                _ => 1.0
+            };
+        }
+
+        private double GetGroundTransportModifier()
+        {
+            return Type switch
+            {
+                WeatherType.Sunny => 1.0,   // Солнечно - никаких изменений
+                WeatherType.Rainy => 0.7,   // Дождь снижает скорость
+                WeatherType.Windy => 0.9,   // Ветер немного снижает скорость
+                WeatherType.Foggy => 0.5,   // Туман снижает скорость из-за плохой видимости
+                _ => 1.0
+            };
+        }
+
+        private double GetAirTransportModifier()
+        {
+            return Type switch
+            {
+                WeatherType.Sunny => 1.0,   // Солнечно - никаких изменений
+                WeatherType.Rainy => 1.0,   // Дождь - никаких изменений
+                WeatherType.Windy => 0.6,   // Сильный ветер замедляет воздушные ТС
+                WeatherType.Foggy => 0.5,   // Туман снижает скорость из-за плохой видимости
+                _ => 1.0
+            };
+        }
+    }
+}
